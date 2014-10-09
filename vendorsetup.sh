b5304c7aef171273258e977abe7f8059e9ac9a7f
Xiaoji <279902746@qq.com>
2014-10-09 08:51:21 +0800

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -14,6 +14,6 @@
 # limitations under the License.
 #
 
-add_lunch_combo wiko-eng
-add_lunch_combo wiko-userdebug
-add_lunch_combo cm_wiko-userdebug
+add_lunch_combo g750_t01-eng
+add_lunch_combo g750_t01-userdebug
+add_lunch_combo cm_g750_t01-userdebug
