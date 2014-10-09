1009a57292630c4edfb7eb64d6b237220525592a
Xiaoji <279902746@qq.com>
2014-10-09 08:44:27 +0800

    Update cm.mk

--- cm.mk
+++ cm.mk
@@ -2,7 +2,7 @@
 TARGET_SCREEN_HEIGHT := 1280
 TARGET_SCREEN_WIDTH := 720
 
-$(call inherit-product, device/mediatek/wiko/device_wiko.mk)
+$(call inherit-product, device/mediatek/g750_t01/device_g750_t01o.mk)
 
 # Inherit some common CM stuff.
 $(call inherit-product, vendor/cm/config/common_mini_phone.mk)
@@ -11,8 +11,8 @@ $(call inherit-product, vendor/cm/config/common_mini_phone.mk)
 $(call inherit-product, vendor/cm/config/gsm.mk)
 
 ## Device identifier. This must come after all inclusions
-PRODUCT_DEVICE := wiko
-PRODUCT_NAME := cm_wiko
-PRODUCT_BRAND := wiko
-PRODUCT_MODEL := wiko
-PRODUCT_MANUFACTURER := wiko
+PRODUCT_DEVICE := g750_t01
+PRODUCT_NAME := cm_g750_t01
+PRODUCT_BRAND := g750_t01
+PRODUCT_MODEL := g750_t01
+PRODUCT_MANUFACTURER := g750_t01
