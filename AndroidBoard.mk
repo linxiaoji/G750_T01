c648069c61e1047205c471dd689d6357c9729614
Xiaoji <279902746@qq.com>
2014-10-09 08:59:59 +0800

    Update AndroidBoard.mk

--- AndroidBoard.mk
+++ AndroidBoard.mk
@@ -5,4 +5,4 @@ include $(CLEAR_VARS)
 ALL_PREBUILT += $(INSTALLED_KERNEL_TARGET)
 
 # include the non-open-source counterpart to this file
--include vendor/mediatek/wiko/AndroidBoardVendor.mk
+-include vendor/mediatek/g750_t01/AndroidBoardVendor.mk
